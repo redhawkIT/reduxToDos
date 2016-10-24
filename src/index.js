79d7009b7093cbb34420a7527062ce8fdc23a88d
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-24 09:33:17 -0500

    working example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import {render} from 'react-dom'
-import App from './App'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import todoApp from './reducers/index'
+import App from './components/App'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
 import './index.css'
 
-render(
-  <App />,
-  document.getElementById('root')
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+let store = createStore(todoApp)
+
+const Main = () => (
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>
 )
+
+render(
+  <Main/>, document.getElementById('root'))
